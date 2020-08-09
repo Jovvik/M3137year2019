3f842e43450d88d9585b291777af546d39b1a1bc
Maxim Mikhaylov <mmikhaylov56@gmail.com>
2020-08-09 21:30:36 +0300

    switched to building everything it takes 2 mins on my machine, no way i can run out of time

--- build.sh
+++ build.sh
@@ -1,13 +1,5 @@
 #!/usr/bin/env bash
 REPOPATH=$(pwd)
-while IFS= read -r -d '' file
-do
-    echo "Building" "$file"
-    cd "$(dirname "$file")"
-    xelatex "$(basename "$file")" > "$REPOPATH"/latex.log
-    xelatex "$(basename "$file")" > "$REPOPATH"/latex.log
-    cd "$REPOPATH"
-done <   <(git diff --name-only HEAD $(git show 'HEAD^{/Built pdfs}' --pretty=format:"%h" --no-patch || git rev-list --max-parents=0 HEAD) | grep "\.tex" | tr '\n' '\0')
 
 while IFS= read -r -d '' file
 do
@@ -16,4 +8,4 @@ do
     xelatex "$(basename "$file")" > "$REPOPATH"/latex.log
     xelatex "$(basename "$file")" > "$REPOPATH"/latex.log
     cd "$REPOPATH"
-done <   <(find . -regex ".*\(final\|opros.*\)\.tex$" -type f -print0)
+done <   <(find . -name "*\.tex" -type f -print0)

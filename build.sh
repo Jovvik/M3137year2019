9c985b41e7e50d36d4808fd902f07f602d02792a
Maxim Mikhaylov <mmikhaylov56@gmail.com>
2020-09-25 21:11:20 +0300

    build fix

--- build.sh
+++ build.sh
@@ -9,12 +9,13 @@ function compile {
 }
 
 REPOPATH=$(pwd)
-while IFS= read -r -d '' file
-do
-    compile "$file"
-done <   <(git diff --name-only HEAD $(git show 'HEAD^{/Built pdfs}' --pretty=format:"%h" --no-patch || git rev-list --max-parents=0 HEAD) | grep "\.tex" | tr '\n' '\0')
 
 while IFS= read -r -d '' file
 do
     compile "$file"
 done <   <(find . -regex ".*\(final\|opros.*\)\.tex$" -type f -print0)
+
+while IFS= read -r -d '' file
+do
+    compile "$file"
+done <   <(git diff --name-only HEAD "$(git show 'HEAD^{/Built pdfs}' --pretty=format:"%h" --no-patch || git rev-list --max-parents=0 HEAD)" | grep "\.tex" | tr '\n' '\0')

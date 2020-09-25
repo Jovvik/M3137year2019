7b239de90e51a42d37d3672461259bcc1a238ecb
Maxim Mikhaylov <mmikhaylov56@gmail.com>
2020-09-25 15:37:36 +0300

    build: started compiling only modified files because repo size was getting too huge

--- build.sh
+++ build.sh
@@ -1,11 +1,20 @@
 #!/usr/bin/env bash
+
+function compile {
+    echo "Building" "$1"
+    cd "$(dirname "$1")"
+    xelatex "$(basename "$1")" > "$REPOPATH"/latex.log
+    xelatex "$(basename "$1")" > "$REPOPATH"/latex.log
+    cd "$REPOPATH"
+}
+
 REPOPATH=$(pwd)
+while IFS= read -r -d '' file
+do
+    compile "$file"
+done <   <(git diff --name-only HEAD $(git show 'HEAD^{/Built pdfs}' --pretty=format:"%h" --no-patch || git rev-list --max-parents=0 HEAD) | grep "\.tex" | tr '\n' '\0')
 
 while IFS= read -r -d '' file
 do
-    echo "Building" "$file"
-    cd "$(dirname "$file")"
-    xelatex "$(basename "$file")" > "$REPOPATH"/latex.log
-    xelatex "$(basename "$file")" > "$REPOPATH"/latex.log
-    cd "$REPOPATH"
-done <   <(find . -name "*\.tex" -type f -print0)
+    compile "$file"
+done <   <(find . -regex ".*\(final\|opros.*\)\.tex$" -type f -print0)

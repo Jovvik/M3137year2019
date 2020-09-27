ba26d44236ab46c2ef2936cdbedc6871da649556
Maxim Mikhaylov <mmikhaylov56@gmail.com>
2020-09-27 20:42:29 +0300

    back to efficient compiling

--- build.sh
+++ build.sh
@@ -3,8 +3,8 @@
 function compile {
     echo "Building" "$1"
     cd "$(dirname "$1")"
-    xelatex "$(basename "$1")" > "$REPOPATH"/latex.log
-    xelatex "$(basename "$1")" > "$REPOPATH"/latex.log
+    xelatex "$(basename "$1")" > "$REPOPATH"/latex.log || exit 1
+    xelatex "$(basename "$1")" > "$REPOPATH"/latex.log || exit 1
     cd "$REPOPATH"
 }
 
@@ -13,14 +13,9 @@ REPOPATH=$(pwd)
 while IFS= read -r -d '' file
 do
     compile "$file"
-done <   <(find . -name "*\.tex" -type f -print0)
+done <   <(find . -regex ".*\(final\|opros.*\)\.tex$" -type f -print0)
 
-# while IFS= read -r -d '' file
-# do
-#     compile "$file"
-# done <   <(find . -regex ".*\(final\|opros.*\)\.tex$" -type f -print0)
-
-# while IFS= read -r -d '' file
-# do
-#     compile "$file"
-# done <   <(git diff --name-only HEAD "$(git show 'HEAD^{/^Built pdfs}' --pretty=format:"%h" --no-patch || git rev-list --max-parents=0 HEAD)" | grep "\.tex" | tr '\n' '\0')
+while IFS= read -r -d '' file
+do
+    compile "$file"
+done <   <(git diff --name-only HEAD "$(git show 'HEAD^{/^Built pdfs}' --pretty=format:"%h" --no-patch || git rev-list --max-parents=0 HEAD)" | grep "\.tex" | tr '\n' '\0')

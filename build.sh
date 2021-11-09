5aa74449097dd4b545b67570512bad02034f303d
Maxim Mikhaylov <mmikhaylov56@gmail.com>
2021-11-09 19:31:26 +0300

    build: try to fix error

--- build.sh
+++ build.sh
@@ -4,10 +4,12 @@ function compile {
     echo "Building" "$1"
     cd "$(dirname "$1")"
     # for _ in {1..20}; do latexmk -f -xelatex -shell-escape "$(basename "$1")" &> /dev/null; done
+    for _ in {1..20}; do
     if ! latexmk -pdf -xelatex -interaction=nonstopmode -shell-escape "$(basename "$1")" &> "$REPOPATH"/latex.log; then
         cat "$REPOPATH"/latex.log
         exit 1
     fi
+    done
     cd "$REPOPATH"
 }
 
